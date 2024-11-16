a6cd7a56c2ca8bb99a78498466152e6cecedc4bf
Ura <mikeneko41@icloudd.com>
2024-11-16 19:44:55 +0900

    session変更

--- next-project/src/app/api/auth/sessionCheck/route.ts
+++ next-project/src/app/api/auth/sessionCheck/route.ts
@@ -1,28 +1,9 @@
 // src/app/api/auth/sessionCheck/route.ts
 
-import getServerSession, { NextAuthResult } from "next-auth";
-import { auth } from "@/auth"
-import { CustomSession, ExtendedToken, options, checkAuthorization } from "../[...nextauth]/authOptions"; // authOptionsをインポート
 import { NextResponse } from "next/server";
+import { getSession, getUserEmail, isAuthenticated } from "./sessionUtils";
 
-export async function getSession() {
-	const session = await auth();
-	if (session && session.user && session.user.email) {
-		return { authentication: "Authenticated", session: session };
-	} else {
-		return { authentication: "Not authenticated", session: null };
-	}
-}
-
-export async function getUserEmail() {
-  const session = await auth();
-  if (session && session.user && session.user.email) {
-    return session.user.email;
-  } else {
-    return "example@example.com";
-  }
-}
-
-export async function isAuthenticated(){
-  return await getSession().then((session) => session?.authentication === "Authenticated");
+export async function GET() {
+  const session = await getSession();
+  return NextResponse.json(session);
 }
