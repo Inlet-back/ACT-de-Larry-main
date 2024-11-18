e9684070c98982b8dfd7c553a37b2aa54df4ff80
Ura <mikeneko41@icloudd.com>
2024-11-18 20:04:51 +0900

    環境変数確認

--- next-project/src/app/api/auth/[...nextauth]/authOptions.ts
+++ next-project/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -11,6 +11,13 @@ export interface ExtendedToken extends JWT {
   user?: User;
 }
 
+// 環境変数のログ出力
+console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+console.log("AUTH_TRUST_HOST:", process.env.AUTH_TRUST_HOST);
+
 export const options: NextAuthConfig = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
