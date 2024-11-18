99754d480ae5e6719c9b4296fea803d062025e64
Ura <mikeneko41@icloudd.com>
2024-11-18 20:42:21 +0900

    ログ追加

--- next-project/src/app/api/auth/[...nextauth]/route.ts
+++ next-project/src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,12 @@ import { handlers } from "../../../../auth";
 // import { options } from "@/app/api/auth/[...nextauth]/authOptions";
 // import NextAuth from "next-auth";
 
+// 環境変数のログ出力
+console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+console.log("AUTH_TRUST_HOST:", process.env.AUTH_TRUST_HOST);
+
 // const handler = NextAuth(options);
 export const { GET, POST } = handlers;
