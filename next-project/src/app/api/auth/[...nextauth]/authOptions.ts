ec303d2666d29c09603da712a5dcef2b48eff39f
Ura <mikeneko41@icloudd.com>
2024-11-18 21:04:53 +0900

    環境変数の変更とオプションのコメントアウト

--- next-project/src/app/api/auth/[...nextauth]/authOptions.ts
+++ next-project/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,71 +1,66 @@
-import NextAuth, { NextAuthConfig, Session, User } from 'next-auth';
-import { JWT } from 'next-auth/jwt';
-import CogntioProvider from 'next-auth/providers/cognito';
-import GoogleProvider from "next-auth/providers/google";
-export interface CustomSession extends Session {
-  accessToken?: string;
-}
+// import NextAuth, { NextAuthConfig, Session, User } from 'next-auth';
+// import { JWT } from 'next-auth/jwt';
+// import CogntioProvider from 'next-auth/providers/cognito';
+// import GoogleProvider from "next-auth/providers/google";
+// export interface CustomSession extends Session {
+//   accessToken?: string;
+// }
 
-export interface ExtendedToken extends JWT {
-  accessToken?: string;
-  user?: User;
-}
+// export interface ExtendedToken extends JWT {
+//   accessToken?: string;
+//   user?: User;
+// }
 
-// 環境変数のログ出力
-console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
-console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
-console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
-console.log("AUTH_TRUST_HOST:", process.env.AUTH_TRUST_HOST);
 
-export const options: NextAuthConfig = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  trustHost: true, // 追加
-  callbacks: {
-    authorized({ request, auth }) {
-      try {
-        return Promise.resolve(!!auth);
-      } catch (error) {
-        return Promise.resolve(false);
-      }
-    },
-    async jwt({ token, account, profile, user }) {
-      const extendedToken: ExtendedToken = token as ExtendedToken;
-      if (account) {
-        extendedToken.accessToken = account.access_token;
-      }
-      if (user) {
-        extendedToken.user = user; // user情報をトークンに追加
-      }
-      return { ...extendedToken, ...user };
-    },
-    async session({ session, token}) {
-      const extendedToken: ExtendedToken = token as ExtendedToken;
-      const customSession: CustomSession = { ...session, accessToken: extendedToken.accessToken, user: extendedToken.user, };
-      return customSession;
-    },
-  },
-  session: {
-    strategy: 'jwt',
-  },
 
-  pages: {
-    signIn: '/login',
-  },
-};
+// export const options: NextAuthConfig = {
+//   secret: process.env.NEXTAUTH_SECRET,
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.AUTH_GOOGLE_ID!,
+//       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+//     }),
+//   ],
+//   trustHost: true, // 追加
+//   callbacks: {
+//     authorized({ request, auth }) {
+//       try {
+//         return Promise.resolve(!!auth);
+//       } catch (error) {
+//         return Promise.resolve(false);
+//       }
+//     },
+//     async jwt({ token, account, profile, user }) {
+//       const extendedToken: ExtendedToken = token as ExtendedToken;
+//       if (account) {
+//         extendedToken.accessToken = account.access_token;
+//       }
+//       if (user) {
+//         extendedToken.user = user; // user情報をトークンに追加
+//       }
+//       return { ...extendedToken, ...user };
+//     },
+//     async session({ session, token}) {
+//       const extendedToken: ExtendedToken = token as ExtendedToken;
+//       const customSession: CustomSession = { ...session, accessToken: extendedToken.accessToken, user: extendedToken.user, };
+//       return customSession;
+//     },
+//   },
+//   session: {
+//     strategy: 'jwt',
+//   },
 
-export const { handlers, auth, signIn, signOut } = NextAuth(options);
+//   pages: {
+//     signIn: '/login',
+//   },
+// };
 
-export function checkAuthorization(authData: any): boolean {
-  try {
-    return !!authData; // authorizedと同じロジックを使う
-  } catch (error) {
-    return false;
-  }
-}
+// export const { handlers, auth, signIn, signOut } = NextAuth(options);
+
+// export function checkAuthorization(authData: any): boolean {
+//   try {
+//     return !!authData; // authorizedと同じロジックを使う
+//   } catch (error) {
+//     return false;
+//   }
+// }
--- next-project/src/app/api/auth/[...nextauth]/route.ts
+++ next-project/src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ import { handlers } from "../../../../auth";
 // 環境変数のログ出力
 console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
 console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
-console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+console.log("AUTH_GOOGLE_ID:", process.env.AUTH_GOOGLE_ID);
+console.log("AUTH_GOOGLE_SECRET:", process.env.AUTH_GOOGLE_SECRET);
 console.log("AUTH_TRUST_HOST:", process.env.AUTH_TRUST_HOST);
 
 // const handler = NextAuth(options);
