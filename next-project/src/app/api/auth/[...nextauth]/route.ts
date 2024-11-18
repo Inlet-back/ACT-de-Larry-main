c78eeedc228e63ca490c681c27b2d89243748d90
Ura <mikeneko41@icloudd.com>
2024-11-18 21:21:35 +0900

    無駄なauthログとオプション削除

--- next-project/src/app/api/auth/[...nextauth]/route.ts
+++ next-project/src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,3 @@
 import { handlers } from "../../../../auth";
-// import { options } from "@/app/api/auth/[...nextauth]/authOptions";
-// import NextAuth from "next-auth";
 
-// 環境変数のログ出力
-console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
-console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
-console.log("AUTH_GOOGLE_ID:", process.env.AUTH_GOOGLE_ID);
-console.log("AUTH_GOOGLE_SECRET:", process.env.AUTH_GOOGLE_SECRET);
-console.log("AUTH_TRUST_HOST:", process.env.AUTH_TRUST_HOST);
-
-// const handler = NextAuth(options);
 export const { GET, POST } = handlers;
